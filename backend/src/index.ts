863ba4172d8ee348d184c1b5c9be5fb5bc827a07
NehGuk <nurture.tumulus_0n@icloud.com>
2026-06-20 21:52:18 +0200

    Implements first functioning version fetching data from DB

---
+++ backend/src/index.ts
@@ -0,0 +1,13 @@
+import "dotenv/config"
+import express from "express"
+import prisma from "./prisma"
+
+const app = express()
+app.use(express.json())
+
+app.get("/", async (req, res) => {
+  const users = await prisma.games.findMany()
+  res.json(users)
+})
+
+app.listen(3000, () => console.log("Running on http://localhost:3000"))
